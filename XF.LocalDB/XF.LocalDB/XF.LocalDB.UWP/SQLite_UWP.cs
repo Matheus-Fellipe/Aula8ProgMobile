5bcefbf3ece7aee109c34f84370d0423eb493d27
Matheus Fellipe Oliveira Cardoso <mathvader09@gmail.com>
2019-11-03 18:50:34 -0300

    Aula8

---
+++ XF.LocalDB/XF.LocalDB/XF.LocalDB.UWP/SQLite_UWP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Xamarin.Forms;
+using XF.LocalDB.Data;
+using XF.LocalDB.UWP;
+
+[assembly: Dependency(typeof(SQLite_UWP))]
+namespace XF.LocalDB.UWP
+{
+    public class SQLite_UWP : ISQLite
+    {
+        public SQLite_UWP() { }
+        public SQLite.SQLiteConnection GetConexao()
+        {
+            var arquivodb = "fiapdb.db3";
+            string caminho = Path.Combine( ApplicationData.Current.LocalFolder.Path, arquivodb);
+            var conexao = new SQLite.SQLiteConnection(caminho);
+            return conexao;
+        }
+    }
+}
---
+++ XF.LocalDB/XF.LocalDB/XF.LocalDB/View/Aluno/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using XF.LocalDB.AlunoViewModel;
+using XF.LocalDB.View.Aluno;
+
+public partial class MainPage : ContentPage
+{
+    AlunoViewModel vmAluno;
+    public MainPage()
+    {
+        vmAluno = new AlunoViewModel();
+        BindingContext = vmAluno;
+        InitializeComponent();
+    }
+    protected override void OnAppearing()
+    {
+        vmAluno = new AlunoViewModel();
+        BindingContext = vmAluno;
+        base.OnAppearing();
+    }
+    private void OnNovo(object sender, EventArgs args)
+    {
+        Navigation.PushAsync(new NovoAlunoView());
+    }
+    private void OnAlunoTapped(object sender,
+    ItemTappedEventArgs args)
+    {
+        var selecionado = args.Item as XF.LocalDB.Model.Aluno;
+        DisplayAlert("Aluno selecionado", "Aluno: " +
+        selecionado.Id, "OK");
+    }
+}
